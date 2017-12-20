7d9b17c9fa9da69b048d59d8fc765bb5577045b2
Jiesi Ren <jiesiren@gmail.com>
2017-12-19 21:32:30 -0500

    Add MessageDetailView component.

---
+++ src/components/MessageDetailView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './MessageDetailView.css';
+
+class MessageDetailView extends Component {
+  render() {
+    return (
+      <div className="MessageDetailView-container" />
+    );
+  }
+}
+
+export default MessageDetailView;
