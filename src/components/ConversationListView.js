8996d4528d0320dac377e539fab34cbbd97eae80
Kevin Lu <jw_lu@hotmail.com>
2017-12-25 18:45:35 -0500

    Merged

---
+++ src/components/ConversationListView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ConversationListView.css';
+
+class ConversationListView extends Component {
+	render() {
+		return (
+			<div className="ConversationListView-spin">Conversations</div>
+		);
+	}
+}
+
+export default ConversationListView;
